0c94fec1fd4d9cd636bdff05b3abf9ab3782689b
eunseoKim <eunseokim43@gmail.com>
2024-12-25 20:23:50 +0900

    [Bronze II] Title: 나머지, Time: 172 ms, Memory: 17720 KB -BaekjoonHub

---
+++ 백준/Bronze/3052. 나머지/나머지.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) {
+        int[] arr = new int[42];
+        for (int i = 0; i < arr.length; i++) {
+            arr[i] = i;
+        }
+
+        int cnt = 10;
+
+        Scanner scanner = new Scanner(System.in);
+        for (int i = 0; i < cnt; i++) {
+            int target = scanner.nextInt();
+            arr[target%42] = -1;
+        }
+
+        int result = 0;
+
+        for (int i : arr) {
+            if(i == -1) {
+                result++;
+            }
+        }
+
+        System.out.println(result);
+
+        scanner.close();
+    }
+}
