ebf423a1a6c2f66cb98a9857ec1033fc6846fe99
eunseoKim <eunseokim43@gmail.com>
2024-12-25 20:27:46 +0900

    [Bronze V] Title: A+B - 7, Time: 224 ms, Memory: 18364 KB -BaekjoonHub

---
+++ 백준/Bronze/11021. A＋B － 7/A＋B － 7.java
@@ -0,0 +1,22 @@
+import java.io.*;
+import java.util.Scanner;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        int[] b = new int[a];
+        int[] c = new int[a];
+
+        for (int i = 0; i < a; i++) {
+            b[i] = sc.nextInt();
+            c[i] = sc.nextInt();
+            sc.nextLine();
+        }
+
+        for (int i = 0; i < a; i++) {
+            System.out.printf("Case #%d: %d", i+1, b[i] + c[i]);
+            System.out.println();
+        }
+    }
+}
