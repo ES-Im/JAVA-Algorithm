aae7b245f431b5dde1a6277d195b0ba420196a00
eunseoKim <eunseokim43@gmail.com>
2024-12-29 15:52:04 +0900

    [Bronze I] Title: 단어 공부, Time: 620 ms, Memory: 34604 KB -BaekjoonHub

---
+++ 백준/Bronze/1157. 단어 공부/단어 공부.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        char[] input = scanner.nextLine().toUpperCase().toCharArray();
+        Set<Character> set = new HashSet<>();
+        for (char c : input) {
+            set.add(c);
+        }
+        Map<Character, Integer> map = new HashMap<>();
+        for (Character c : set) {
+            int cnt = 0;
+            for (char i : input) {
+                if(c == i) cnt++;
+            }
+            map.put(c, cnt);
+        }
+
+        List<Map.Entry<Character, Integer>> m = new ArrayList<>(map.entrySet());
+        m.sort((entry1, entry2) -> {
+            int descVal = entry2.getValue().compareTo(entry1.getValue());
+            return descVal;
+        });
+
+        if(m.size() > 1 && m.get(0).getValue().equals(m.get(1).getValue())) {
+            System.out.println("?");
+        } else {
+            System.out.println(m.get(0).getKey());
+        }
+
+        scanner.close();
+
+    }
+}
