56f351e87dac0ac9196bd5c30cd5e618d16df172
eunseoKim <eunseokim43@gmail.com>
2025-09-22 22:07:58 +0900

    [level 2] Title: 올바른 괄호, Time: 20.52 ms, Memory: 55.1 MB -BaekjoonHub

---
+++ 프로그래머스/2/12909. 올바른 괄호/올바른 괄호.java
@@ -0,0 +1,20 @@
+import java.util.*;
+
+class Solution {
+    boolean solution(String s) {
+        boolean answer = true;
+        Stack<Character> stack = new Stack<>();
+        
+        for(char c : s.toCharArray()) {
+            if(c == '(') stack.push(c);
+            if(c == ')') {
+                if(stack.isEmpty()) return false;
+                else stack.pop();
+            } 
+        }
+            
+        if(stack.size() != 0) answer = false;
+
+        return answer;
+    }
+}
