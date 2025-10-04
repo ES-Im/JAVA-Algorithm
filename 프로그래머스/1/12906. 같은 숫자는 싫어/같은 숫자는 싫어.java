1fa7a8dcb67878159a0edb4fffe56c6de1efeb92
eunseoKim <eunseokim43@gmail.com>
2025-10-04 10:23:37 +0900

    [level 1] Title: 같은 숫자는 싫어, Time: 51.37 ms, Memory: 118 MB -BaekjoonHub

---
+++ 프로그래머스/1/12906. 같은 숫자는 싫어/같은 숫자는 싫어.java
@@ -0,0 +1,19 @@
+import java.util.*;
+
+public class Solution {
+    public int[] solution(int[] arr) {
+        Stack<Integer> stack = new Stack<>();
+        stack.add(arr[0]);
+        
+        for(int i = 1; i < arr.length; i++) {
+            if(stack.peek() == arr[i]) continue;
+            else stack.add(arr[i]);
+        }
+        
+        int i = 0;
+        int[] answer = stack.stream().mapToInt(Integer::intValue).toArray();
+            
+        
+        return answer;
+    }
+}
