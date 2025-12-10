a3ede5c33ae7242e1052045c430da0ee5b28e708
eunseoKim <eunseokim43@gmail.com>
2025-12-10 10:34:57 +0900

    [level 2] Title: 의상, Time: 0.17 ms, Memory: 88.7 MB -BaekjoonHub

--- 프로그래머스/2/42578. 의상/의상.java
+++ 프로그래머스/2/42578. 의상/의상.java
@@ -2,23 +2,44 @@ import java.util.*;
 
 class Solution {
     public int solution(String[][] clothes) {
-        int answer = 1;
-        HashMap<String, Integer> coni = new HashMap<>();
+        HashMap<String, Integer> match = new HashMap<>();
         
-        for(int i = 0; i < clothes.length; i++) {
-            String category = clothes[i][1];
-            coni.put(category, coni.getOrDefault(category, 1)+1);   // 안입었을때 경우의 수 추가 (디폴트 : +1)
+        for(String[] cloth : clothes) {
+            match.put(cloth[1], match.getOrDefault(cloth[1], 0) + 1);
         }
         
-        // for(String key : coni.keySet()) {
-        //     System.out.println(key + " : " + coni.get(key));
-        // }
+        int result = 1;
         
-        for(Integer cnt : coni.values()) {
-            answer *= cnt;            
+        for(Integer i : match.values()) {
+            result *= (i+1);
         }
         
-        
-        return answer - 1;
+        return result-1;
     }
 }
+
+// 이전코드
+// import java.util.*;
+
+// class Solution {
+//     public int solution(String[][] clothes) {
+//         int answer = 1;
+//         HashMap<String, Integer> coni = new HashMap<>();
+        
+//         for(int i = 0; i < clothes.length; i++) {
+//             String category = clothes[i][1];
+//             coni.put(category, coni.getOrDefault(category, 1)+1);   // 안입었을때 경우의 수 추가 (디폴트 : +1)
+//         }
+        
+//         // for(String key : coni.keySet()) {
+//         //     System.out.println(key + " : " + coni.get(key));
+//         // }
+        
+//         for(Integer cnt : coni.values()) {
+//             answer *= cnt;            
+//         }
+        
+        
+//         return answer - 1;
+//     }
+// }
